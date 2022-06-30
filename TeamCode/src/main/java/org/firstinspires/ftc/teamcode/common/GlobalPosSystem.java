6986c882fa10ea0a1df3a834deae089d08db5c33
panda1771 <85645315+panda1771@users.noreply.github.com>
2022-06-29 18:18:47 -0700

    maybe fix...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/GlobalPosSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/GlobalPosSystem.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.auto.gps;
+package org.firstinspires.ftc.teamcode.common;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
