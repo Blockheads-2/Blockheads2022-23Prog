a6bc7383aa10aa688b8b89d28925357bcb53158c
DaiTou567 <vh95vvhh@gmail.com>
2022-07-02 13:41:02 -0700

    worked on linear motion var head

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/kinematics/LinearMotionVariableHeading.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/kinematics/LinearMotionVariableHeading.java
@@ -1,5 +1,12 @@
 package org.firstinspires.ftc.teamcode.auto.kinematics;
 
-public class LinearMotionVariableHeading {
+import org.firstinspires.ftc.teamcode.common.Constants;
+import org.firstinspires.ftc.teamcode.common.HardwareDrive;
+import org.firstinspires.ftc.teamcode.common.gps.GlobalPosSystem;
 
+import org.firstinspires.ftc.teamcode.common.pid.RotateSwerveModulePID;
+import org.firstinspires.ftc.teamcode.common.positioning.MathSwerveAuto;
+
+public class LinearMotionVariableHeading {
+    private double x
 }
