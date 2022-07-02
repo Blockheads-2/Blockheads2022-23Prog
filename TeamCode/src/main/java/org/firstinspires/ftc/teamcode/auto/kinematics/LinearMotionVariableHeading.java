cd3efaea2e672513f3e7f73411cf5616c5586b29
DaiTou567 <vh95vvhh@gmail.com>
2022-07-02 14:17:19 -0700

    worked on linearmotionvarhead

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/kinematics/LinearMotionVariableHeading.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/kinematics/LinearMotionVariableHeading.java
@@ -8,5 +8,24 @@ import org.firstinspires.ftc.teamcode.common.pid.RotateSwerveModulePID;
 import org.firstinspires.ftc.teamcode.common.positioning.MathSwerveAuto;
 
 public class LinearMotionVariableHeading {
-    private double x
+    //declaring class objects
+    HardwareDrive robot = new HardwareDrive();
+    Constants constants = new Constants();
+    GlobalPosSystem posSystem = new GlobalPosSystem();
+
+    ////rotation and translate power(for ratio)
+    private double translationPowerPercentage = 0.0;
+    private double rotationPowerPercentage = 0.0;
+
+    //power of module rotation
+    private double rotatePower = 0.0;
+
+    public void linearMotionVariableHeading(){
+
+    }
+
+    private double x = 0;
+    private double y = 0;
+
+
 }
