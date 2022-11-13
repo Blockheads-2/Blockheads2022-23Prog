4922128c37a9ec3e9815713cb821e677962f02cc
CoolJosh40 <jsugino@lacanadaengineeringclub.org>
2022-11-12 19:52:29 -0800

    new revised kinematics that may work ☹️

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/kinematics/drive/actions/Snap.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.common.kinematics.drive.actions;
+
+public class Snap {
+
+    //find target
+
+    //go to position
+        //sync
+        //need better PID values (so it doesn't overshoot and undershoot so much)
+}
