c22d678f8597b8ca2b0f242b0b7d802db7d560a1
panda1771 <85645315+panda1771@users.noreply.github.com>
2022-11-22 15:59:59 -0800

    :(

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/kinematics/drive/RevisedKinematics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/kinematics/drive/RevisedKinematics.java
@@ -38,8 +38,8 @@ public class RevisedKinematics {
     public int leftRotClicks = 0;
     public int spinClicksR = 0; //make protected later
     public int spinClicksL = 0; //make protected later
-    public int rightThrottle = -1;
-    public int leftThrottle = -1;
+    public int rightThrottle = 1;
+    public int leftThrottle = 1;
 
     public double target = 0;
     public double turnAmountL = 0;
